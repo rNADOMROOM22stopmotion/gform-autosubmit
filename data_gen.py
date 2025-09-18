b1050a0226909c1178df1d66bb8db108a67d63e1
dhruv <senpaigaming126@gmail.com>
2025-09-18 20:19:35 +0530

    added random payload generator, ready for testing

---
+++ data_gen.py
@@ -0,0 +1,40 @@
+import json
+import random
+
+with open("fields.json", "r") as file:
+    data = json.load(file)
+
+
+r_data = random.choice(data)
+# o/p : {'name': 'Berry', 'branch': 'ICT', 'sem': 5, 'scale': 4, 'word': 'odio'}
+
+def new_payload():
+    payload = {
+        "entry.1121890835": r_data['name'],
+        "entry.1279575812": r_data['branch'],
+        "entry.2006747066": int(r_data['sem']),
+        "entry.2011057818": [
+            "Mediocre/Outdated Curriculum",
+            "Unclean Washrooms",
+            "Out-dated Lab Equipment",
+            "Malfunctioning Ceiling Fans",
+            "Unsatisfactory Food Quality In Canteen",
+            "__other_option__"
+        ],
+        "entry.2011057818.other_option_response": r_data['word'],
+        "entry.1913878908": int(r_data['scale']),
+    }
+    return payload
+
+# print(new_payload())
+#{'entry.1121890835': 'Aharon',
+# 'entry.1279575812': 'ICT',
+# 'entry.2006747066': 6,
+# 'entry.2011057818': ['Mediocre/Outdated Curriculum',
+# 'Unclean Washrooms',
+# 'Out-dated Lab Equipment',
+# 'Malfunctioning Ceiling Fans',
+# 'Unsatisfactory Food Quality In Canteen',
+# '__other_option__'],
+# 'entry.2011057818.other_option_response': 'et',
+# 'entry.1913878908': 5}
--- main.py
+++ main.py
@@ -1,33 +1,41 @@
 from concurrent.futures import ThreadPoolExecutor
 import requests
 from time import sleep
+from data_gen import new_payload
 
-URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdas_WPB6OzwCGh2pOyaFYkovAUJeE9-7KapjLqTq0xbpkwVA/formResponse?pli=1"
-payload = {
-	"entry.131900708": "testt",
-	"entry.1804986014": "ans1",
-	"entry.1010018223": "Option 1",
-	"entry.1010018223": "Option 2",
-	"entry.1010018223": "Option 3",
-}
+# test URL
+# URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdas_WPB6OzwCGh2pOyaFYkovAUJeE9-7KapjLqTq0xbpkwVA/formResponse?pli=1"
+
+# test payload
+# payload = {
+#     "entry.131900708": "testt",
+#     "entry.1804986014": "ans1",
+#     "entry.1010018223": "Option 1",
+#     "entry.1010018223": "Option 2",
+#     "entry.1010018223": "Option 3",
+# }
+
+URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSefeUcLXD-gKFTxeuzJsbirEnQT-TrIvAcRyGYjnlDC23jLBQ/formResponse?pli=1"
+
+payload = new_payload()
 
 
 def main():
-	while True:
-		sleep(5)
-		r = requests.post(URL, data=payload)
+    for _ in range(1):
+        #sleep(5)
+        r = requests.post(URL, data=payload)
 
-		if r.ok:
-    		print("Submitted successfully!", r.status_code)
-		else:
-    		print("Failed:", r.status_code)
+        if r.ok:
+            print("Submitted successfully!", r.status_code)
+        else:
+            print("Failed:", r.status_code)
 
 def work():
-	num_threads = 20
+    num_threads = 1
 
-	with ThreadPoolExecutor(max_workers=num_threads) as executor:
-		for i in range(num_threads):
-			executor.submit(main)
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        for i in range(num_threads):
+            executor.submit(main)
 
 if __name__ == "__main__":
     work()
